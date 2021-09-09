c42185ca849a3dee3711cf398820f0ed5231b541
mythosmystery <axiysfire@gmail.com>
2021-09-09 15:13:50 -0500

    fixed delay in adding to recently searched list

--- src/components/RecentlySearched.js
+++ src/components/RecentlySearched.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Container, Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 import { removeCity } from '../utils/localStorage';
 const RecentlySearched = ({ setCity, savedCityList, setSavedCityList }) => {
+   const reversedList = savedCityList.reverse();
    return (
       <Container className="my-2">
          <Card>
             <Card.Body>
                <Card.Title>Recently Searched:</Card.Title>
                <ListGroup className="list-group-flush">
-                  {savedCityList.reverse().map(city => {
+                  {reversedList.map(city => {
                      return (
                         <ListGroupItem key={city}>
                            <span onClick={() => setCity(city)}>{city}</span>{' '}
